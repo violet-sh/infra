45fd11e6f03d021e421e4468c4736a62be3ff1e3
tibs <tibs@tibinonest.me>
2025-08-01 02:18:16 -0400

    Update nix-index-database home module

--- presets/home/default.nix
+++ presets/home/default.nix
@@ -2,7 +2,7 @@
 {
   imports = with inputs; [
     catppuccin.homeModules.catppuccin
-    nix-index-database.hmModules.nix-index
+    nix-index-database.homeModules.nix-index
 
     ../../modules/home
 
