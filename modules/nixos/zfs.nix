f64d3b63014dd779352cd9bc83ffb0e6236c0078
tibs <tibs@tibinonest.me>
2025-08-29 10:01:31 -0400

    Fix zfs kernel versioning

--- modules/nixos/zfs.nix
+++ modules/nixos/zfs.nix
@@ -7,15 +7,11 @@
 let
   cfg = config.modules.zfs;
 
-  isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
   zfsCompatibleKernelPackages = lib.filterAttrs (
     name: kernelPackages:
     (builtins.match "linux_[0-9]+_[0-9]+" name) != null
     && (builtins.tryEval kernelPackages).success
-    && (
-      (!isUnstable && !kernelPackages.zfs.meta.broken)
-      || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
-    )
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
   ) pkgs.linuxKernel.packages;
   latestKernelPackage = lib.last (
     lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
