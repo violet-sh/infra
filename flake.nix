080fc1110f8c39e614267cf6b4296058a1ba8d30
violet <vi@violet.sh>
2026-02-01 11:31:43 -0500

    Update nix substituters, cleanup flake

--- flake.nix
+++ flake.nix
@@ -4,11 +4,9 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 
@@ -83,15 +81,8 @@
     {
       self,
       nixpkgs,
-      catppuccin,
       deploy-rs,
-      home-manager,
-      nixos-hardware,
-      nur,
-      ragenix,
-      rust-overlay,
       treefmt-nix,
-      systems,
       ...
     }@inputs:
     let
