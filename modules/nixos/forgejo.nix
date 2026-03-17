f27fd688c1dc10fd1a1dcab8a16f1d1e6bafaa58
violet <vi@violet.sh>
2026-03-17 15:18:02 -0400

    Improve forgejo module config

--- modules/nixos/forgejo.nix
+++ modules/nixos/forgejo.nix
@@ -7,12 +7,33 @@ in
     enable = mkEnableOption { description = "Enable Forgejo"; };
     hostname = mkOption { type = types.str; };
     port = mkOption { type = types.port; };
+    ssh_port = mkOption { type = types.port; };
+    lfs = mkOption {
+      type = types.bool;
+      default = true;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.forgejo = {
       enable = true;
-      settings.server.HTTP_PORT = cfg.port;
+      lfs.enable = cfg.lfs;
+      settings = {
+        APP_NAME = "violet's git";
+        server = {
+          DOMAIN = cfg.hostname;
+          HTTP_PORT = cfg.port;
+          PROTOCOL = "https";
+          ROOT_URL = "https://${cfg.hostname}/";
+          SSH_PORT = cfg.ssh_port;
+        };
+        security = {
+          PASSWORD_HASH_ALGO = "argon2";
+        };
+        ui = {
+          DEFAULT_THEME = "catppuccin-macchiato-lavender";
+        };
+      };
     };
 
     modules.caddy.services.forgejo = {
