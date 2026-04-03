167f92cb8e2b7b601db4689a9d3b3e95cee54ec1
violet <vi@violet.sh>
2026-04-03 14:19:10 -0400

    Improve Caddy snippet handling

--- modules/nixos/caddy.nix
+++ modules/nixos/caddy.nix
@@ -53,6 +53,47 @@ in
       description = "Path to Caddyfile";
       default = null;
     };
+    snippets = mkOption {
+      type = with types; listOf str;
+      default = [
+        ''
+          (global) {
+            encode zstd gzip
+          }
+          (security) {
+            header Content-Security-Policy "default-src 'none'; base-uri 'none'; form-action 'none'; frame-ancestors 'none'"
+           	header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
+           	header X-Content-Type-Options nosniff
+           	header X-XSS-Protection "1; mode=block"
+           	header X-Frame-Options DENY
+           	header Referrer-Policy no-referrer
+           	header Permissions-Policy interest-cohort=()
+           	header Cross-Origin-Embedder-Policy require-corp
+           	header Cross-Origin-Opener-Policy same-origin
+           	header Cross-Origin-Resource-Policy same-origin
+          }
+          (branding) {
+           	header Server Aether
+           	header X-Clacks-Overhead "GNU Jabari Latrell Peoples, Cecile Richards, Nikki Giovanni, Alexei Navalny, Nex Benedict, Andre Braugher, Corei Hall, Ryan Carson, Harry Belafonte, Brianna Ghey, Technoblade, Alice Litman, Axel Matters, John Lewis, Larry Kramer, George Floyd, Toni Morrison, Aretha Franklin, Avicii, Stephen Hawking, Ursula K. Le Guin, Ben Barres, Natalie Nguyen, Debbie Reynolds, Carrie Fisher, Grace Lee Boggs, Terry Pratchett, Eric Garner, Ruby Dee, Maya Angelou, Pete Seeger, Elizabeth Catlett, Trayvon Martin, Denis Ritchie, Miki Endo, Esther Earl, Lena Horne, Dorothy Height, Eartha Kitt, Katherine Dunham, Octavia E. Butler, Coretta Scott King, Rosa Parks, Shirley Chisholm, Ginny Fiennes, Nina Simone, Sylvia Rivera, Douglas Adams, James Farmer, Matthew Shepard, John Denver, Ella Fitzgerald, Jonathan Larson, Cesar Chavez, Audre Lorde, Marsha P. Johnson, Keith Haring, C. L. R. James, James Baldwin, E. D. Nixon, Roy Wilkins, Terry Fox, A. Philip Randolph, Harvey Milk, Phil Ochs, Whitney Young, Judy Garland, Martin Luther King Jr., Woody Guthrie, Langston Hughes, Malcolm X, Sam Cooke, W. E. B. Du Bois, Alan Turing, Alice Stone Blackwell, Carrie Chapman Catt, Jean Tatlock, Lillian Wald, Emma Goldman, Ida B. Wells, Mary Burnett Talbert, Frances Ellen Watkins Harper, Julia Ward Howe, Henry Browne Blackwell, Susan B. Anthony, Elizabeth Cady Stanton, Abigail Bush, Henry Box Brown, Frederick Douglass, Amelia Bloomer, Lucy Stone, Wendell Phillips, Sojourner Truth, Lucretia Mott, William Lloyd Garrison, Charles Sumner, Lindley Murray Moore, James Mott, Margaret Fuller, Abigail Mott, Mary Wollstonecraft"
+          }
+          (cdn) {
+           	header Access-Control-Allow-Methods GET
+           	header Access-Control-Allow-Origin *
+           	header Cross-Origin-Resource-Policy cross-origin
+          }
+          (bunny) {
+          	tls {
+          		dns bunny {$BUNNY_API_KEY}
+          	}
+          }
+          (cloudflare) {
+          	tls {
+          		dns cloudflare {$CLOUDFLARE_API_KEY}
+          	}
+          }
+        ''
+      ];
+    };
     extraConfig = mkOption {
       type = types.lines;
       description = "Extra config to add to the created Caddyfile";
@@ -85,6 +126,8 @@ in
             hostName = value.hostname;
             serverAliases = value.serverAliases;
             extraConfig = ''
+              import global
+              import branding
               import ${value.dnsProvider}
               reverse_proxy ${value.address}:${toString value.port}
             '';
@@ -92,23 +135,9 @@ in
         in
         services;
       configFile = lib.mkIf (cfg.configFile != null) cfg.configFile;
-      extraConfig =
-        let
-          tls = ''
-            (bunny) {
-            	tls {
-            		dns bunny {$BUNNY_API_KEY}
-            	}
-            }
-            (cloudflare) {
-            	tls {
-            		dns cloudflare {$CLOUDFLARE_API_KEY}
-            	}
-            }
-          '';
-        in
-        cfg.extraConfig + tls;
+      extraConfig = cfg.extraConfig + lib.concatStrings cfg.snippets;
       environmentFile = config.age.secrets.caddy_env.path;
+      logFormat = lib.mkForce "level INFO";
     };
 
     modules.prometheus.scrapeConfigs = lib.mkIf cfg.metrics [
