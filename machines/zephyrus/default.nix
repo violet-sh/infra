9fe1ddee7046ca188a271b2bf5f3a7a81345880e
violet <vi@violet.sh>
2026-09-26 11:12:21 -0400

    Get Zephyrus to a working state (hopefully)

--- machines/zephyrus/default.nix
+++ machines/zephyrus/default.nix
@@ -7,7 +7,50 @@
     ../../presets/nixos/server
   ];
 
+  boot = {
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      mirroredBoots = [
+        {
+          devices = [ "nodev" ];
+          path = "/boot";
+        }
+      ];
+    };
+  };
+
+  networking = {
+    hostName = "zephyrus";
+    hostId = "7920a53b";
+    useNetworkd = true;
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "199.127.129.190";
+          prefixLength = 25;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2602:f831::4665:8aff:fee3:5ae8";
+          prefixLength = 64;
+        }
+      ];
+    };
+
+    defaultGateway = {
+      address = "199.127.129.129";
+      interface = "enp1s0";
+    };
+    defaultGateway6 = {
+      address = "2a0a:8dc0:2000:97::1";
+      interface = "enp1s0";
+    };
+  };
+
   # ======================== DO NOT CHANGE THIS ========================
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.11";
   # ======================== DO NOT CHANGE THIS ========================
 }
--- machines/zephyrus/hardware-configuration.nix
+++ machines/zephyrus/hardware-configuration.nix
@@ -21,12 +21,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/46f22931-63b4-45b5-af81-110fecb64526";
+    device = "/dev/disk/by-uuid/5ca89f1c-fe34-4e2c-993c-152cfa825e1f";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/1A41-FBD7";
+    device = "/dev/disk/by-uuid/D1B4-B97B";
     fsType = "vfat";
     options = [
       "dmask=0077"
@@ -34,7 +34,7 @@
     ];
   };
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/6ae76570-5fc2-4e42-8a4e-c0cb19d42ddf"; } ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/23124f35-b5f1-4f7a-abcb-34e2b0f2cac9"; } ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
